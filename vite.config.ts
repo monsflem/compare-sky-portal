1478352f3f5de7ec34fcd39ae4214ffc554f9660
monsflem <monsikflemsik@gmail.com>
2025-05-28 01:41:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/compare-sky-portal/' : '/',
+  base: mode === "production" ? "/compare-sky-portal/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
   build: {
-    assetsDir: 'assets',
+    assetsDir: "assets",
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    }
+        assetFileNames: "assets/[name].[hash].[ext]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
   },
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'dist/index.html',
-          dest: '',
-          rename: '404.html',
-        },
-      ],
-      hook: 'writeBundle',
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
