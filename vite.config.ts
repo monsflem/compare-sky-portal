e121483253ca6381534cfcd99aa63c8557b36e8b
monsflem <monsikflemsik@gmail.com>
2025-05-28 00:28:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Set base path for GitHub Pages deployment
-  base: mode === 'production' ? '/comparison-portal/' : '/',
+  base: mode === 'production' ? '/compare-sky-portal/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
